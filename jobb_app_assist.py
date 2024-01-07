67825e66f7f420d6be5297f851d6e66946f8ce2c
KatjaSi <katja.sivertsen@gmail.com>
2024-01-07 20:42:28 +0100

    en/no

--- jobb_app_assist.py
+++ jobb_app_assist.py
@@ -1,4 +1,5 @@
 import streamlit as st  
+import json
 from langchain.vectorstores import chroma
 
 from data_utils import scrap_from_job_ad, read_pdf
@@ -7,6 +8,24 @@ from time import sleep
 
 from text_generator import generate_application, get_key_info, extract_candidate_info
 
+st.session_state['app_lan_key'] = 0
+
+def load_texts():
+    with open("texts.json", "r", encoding="utf-8") as file:
+        all_texts = json.load(file)
+        st.session_state.all_texts = all_texts
+
+def get_language():
+    lang = st.session_state.language if "language" in st.session_state else "no"
+    return lang
+
+selected_language = get_language()
+
+
+def language_callback(language):
+    st.session_state.language = language
+    st.session_state.app_lan_key = 1
+
 
 def local_css(file_name):
     with open(file_name) as f:
@@ -14,54 +33,70 @@ def local_css(file_name):
 
 local_css("main.css")
 
+all_texts = load_texts()
+
+with st.sidebar:
+    button_container = st.container()
+    button_container.subheader(st.session_state.all_texts["choose_lan_msg"][selected_language])
+    col1, col2, _ = button_container.columns([1,1,2])
+    en_button = col1.button(label="no", type="secondary", use_container_width=True, on_click=language_callback, args=("no",))
+    no_button = col2.button(label="en",type="secondary", use_container_width=True,  on_click=language_callback, args=("en",))
+
 def main():
+    st.title(st.session_state.all_texts['welcome_message'][selected_language])
+    st.divider()
 
-    st.subheader("Trenger du hjelp til å skrive en jobbsøknad?")
 
-    left_col, right_col = st.columns(2, gap="small")
+    st.subheader(st.session_state.all_texts['need_help_message'][selected_language])
+    left_col, right_col = st.columns([3,4])
 
     with st.container():
         with left_col:
             image_orginal = Image.open("img/woman_assistant.png")
-            st.image(image=image_orginal, width=280)
+            st.image(image=image_orginal, use_column_width=True)
 
         with right_col:
-            http_job = st.text_input("Limm inn http av jobbannonsen her:")
-            cv_file = st.file_uploader("Last opp din CV her:", type="pdf")
+            http_job = st.text_input(st.session_state.all_texts['job_http'][selected_language])
+            cv_file = st.file_uploader(st.session_state.all_texts['upload_cv_message'][selected_language], type="pdf")
+            app_language = st.selectbox(st.session_state.all_texts['choose_app_lan_msg'][selected_language],("norsk", "english")) 
               
-        #extra_text = st.text_area(placeholder="Skriv mer om deg selv hvis du har lyst ..", label="Skriv mer:", max_chars=512)
-        clicked = st.button("Generer Søknad!", use_container_width=True, type="primary")
+        #extra_text = st.text_area(placeholder="Skriv mer om deg selv hvis du har lyst ..", label="Skriv mer:", max_chars=512)   
+        clicked = st.button(st.session_state.all_texts['generate_app_message'][selected_language], use_container_width=True, type="primary")
 
         if clicked:
+            if cv_file is None:
+                st.write(st.session_state.all_texts["did_not_get_cv_msg"][selected_language])
+                return
             st.divider()
-            job_info = scrap_from_job_ad(path=http_job)
-    
-            key_info = get_key_info(job_info)
-            #st.subheader("Hiring company:")
-            with st.expander("Hiring Company:"):
+            with st.spinner(st.session_state.all_texts["extract_from_job_app_msg"][selected_language]):
+                # TODO: add error handling here
+                job_info = scrap_from_job_ad(path=http_job)
+                key_info = get_key_info(job_info, language=app_language)
+            
+            with st.expander(st.session_state.all_texts['hiring_company_expander_heading'][selected_language]):
                 st.write(key_info['company_info'])
-            #st.subheader("Key Qualifications Required:")
-            with st.expander("Key Qualifications Required:"):
+                
+            with st.expander(st.session_state.all_texts['qualifications_required_expander_heading'][selected_language]):
                 st.write(key_info['key_qualifications'])
 
             st.divider()
+            with st.spinner(st.session_state.all_texts['fit_for_position_spinner_msg'][selected_language]):
+                sleep(60) # wait one minute to not exceed api limit
             
-
-            sleep(60) # wait one minute to not exceed api limit
-            if cv_file is not None:
                 text = read_pdf(cv_file)
-                info = extract_candidate_info(candidate_info_text=text, job_info=key_info)
-                # st.write(info["name"])
-                with st.expander("Your Qualifications:"):
-                    st.write(info["qualifications"])
+                info = extract_candidate_info(candidate_info_text=text, job_info=key_info, language=app_language)
+            with st.expander(st.session_state.all_texts['your_qualifications_expander_msg'][selected_language]):
+                st.write(info["qualifications"])
                 #st.write(info["current position"])
-                with st.expander("Your Working Experience:"):
-                    st.write(info["years of experience"])
-            sleep(60)   
-            text = generate_application(job_info=key_info, candidate_info=info)
-            st.divider()
-            st.subheader("Cover Letter:")
-            st.text_area("", text, height=800)
+            with st.expander(st.session_state.all_texts['your_working_experience_expander_msg'][selected_language]):
+                st.write(info["years of experience"])
+            with st.spinner(st.session_state.all_texts['generating_app_soinner_msg'][selected_language]):
+                sleep(60)   
+                text = generate_application(job_info=key_info, candidate_info=info, language=app_language)
+                st.divider()
+                st.subheader(st.session_state.all_texts['cover_letter_heading'][selected_language])
+                st.text_area("", text, height=700)
+            
 
 
 if __name__ == "__main__":
--- text_generator.py
+++ text_generator.py
@@ -1,5 +1,5 @@
 import datetime
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv, find_dotenv
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
@@ -14,43 +14,64 @@ from datetime import datetime
 
 load_dotenv(find_dotenv(), override=True)
 
-#def generate_application(company, position, candidate_description):
-def generate_application(job_info, candidate_info):
-    template = f" Write a cover letter in norwegian language\
+LANGUAGE_VALUES = {
+    "no": "Norwegian",
+    "en": "English",
+    "norsk": "Norwegian",
+    "english": "English"
+}
+
+def generate_application(job_info, candidate_info, language):
+    language = LANGUAGE_VALUES[language]
+    eng_template = f" Write a cover letter in norwegian language\
           to apply for a postion {job_info['position']} with a following requirements: {job_info['key_qualifications']} in company with describtion: {job_info['company_info']}\
               for a candidate with the folowing qualifications: {candidate_info['qualifications']}.\
                 Take into consideration this information about candidate experience: {candidate_info['years of experience']}. Candidate name is {candidate_info['name']}.\
                     Answer the question: 'Why should you hire me?'"
-
+    no_template = f"Generer et søknadsbrev på norsk språk for å søke på stillingen {job_info['position']} med følgende krav: {job_info['key_qualifications']} \
+        i selskapet med beskrivelse: {job_info['company_info']} for en kandidat med følgende kvalifikasjoner: {candidate_info['qualifications']}. \
+            Ta hensyn til denne informasjonen om kandidatens erfaring: \
+        {candidate_info['years of experience']}. Kandidatens navn er {candidate_info['name']}. Besvar spørsmålet: 'Hvorfor bør du ansette meg?'"
     prompt = PromptTemplate(
         input_variables=['position', 'requirements', 'company', 'candidate_description', 'experience','name'],
-        template=template
+        template=eng_template if language=="English" else no_template
     )
 
-    llm = OpenAI(model_name="text-davinci-003", temperature=0.7, max_tokens=1500)
+    llm = OpenAI(model_name="text-davinci-003", temperature=0.6, max_tokens=1024)
     output = llm(prompt.format( job_info=job_info, candidate_info=candidate_info))
     
     return output
 
 
-def get_key_info(job_info):
-    # qualifications && about company
+def get_key_info(job_info, language):
+    language = LANGUAGE_VALUES[language]
     key_info = dict(())
     key_info.update(job_info)
+
+    human_message_en = f"Provide main key qualifications required for the job from the following job advertisement: {job_info['ad_text']}. \
+                     Provide the answer as a list with each qualification in one line."
+    human_message_no = f"Gi de viktigste nøkkelkvalifikasjonene som kreves for jobben fra følgende stillingsannonse: {job_info['ad_text']}. \
+        Gi svaret som en liste med hver kvalifikasjon på en linje."
+
     messages_1 = [
         SystemMessage(content="You are expert in summirizing the key points of job advertisements."),
-        HumanMessage(content=f"Provide main key qualifications required for the job from the following job advertisement: {job_info['ad_text']}. \
-                     Provide the answer as a list with each qualification in one line.")
+        SystemMessage(content = f"Provide text in {language} language."),
+        HumanMessage(content=human_message_en if language=="English" else human_message_no )
     ]
 
     llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
     key_qualifications = llm(messages_1).content
     key_info['key_qualifications']=key_qualifications
 
+    human_message_en= f"Provide the most relevant information about the hiring company from the folowing job advertisement: {job_info['extended_ad_text']}. \
+                     The answer should be in 5-6 sentences and only contain the information about the company itself."
+    human_message_no = f"Gi den mest relevante informasjonen om selskapet som rekrutterer fra følgende stillingsannonse: {job_info['extended_ad_text']}. \
+        Svaret bør være på 5-6 setninger og kun inneholde informasjon om selve selskapet."
+
     messages_2 = [
         SystemMessage(content="You are expert in summirizing the key points of job advertisements."),
-        HumanMessage(content=f"Provide the most relevant information about the hiring company from the folowing job advertisement: {job_info['extended_ad_text']}. \
-                     The answer should be in 5-6 sentences and only contain the information about the company itself.")
+        SystemMessage(content = f"Provide text in {language} language."),
+        HumanMessage(content=human_message_en if language=="English" else human_message_no )
     ]
 
     company_info = llm(messages_2).content
@@ -59,7 +80,8 @@ def get_key_info(job_info):
 
     return key_info
 
-def extract_candidate_info(candidate_info_text, job_info):
+def extract_candidate_info(candidate_info_text, job_info, language):
+    language = LANGUAGE_VALUES[language]
     info = dict(())
     messages = [
         SystemMessage(content="You are expert in summirizing the key points of CV. "),
@@ -71,44 +93,63 @@ def extract_candidate_info(candidate_info_text, job_info):
     name = llm(messages).content
     info["name"] = name
 
-    human_message = f"Extract the candidate's main qualifications from the provided CV snippet (not more than six) : {candidate_info_text}. \
-                   Provide the answer as a list with each qualification in one line in english language."
+    human_message_en = f"Extract the candidate's main qualifications from the provided CV snippet (not more than six) : {candidate_info_text}. \
+                   Provide the answer as a list with each qualification in one line."
+    human_message_no = f"Finn kandidatens viktigste kvalifikasjoner fra den gitte CV-utdragsbiten (ikke mer enn seks): {candidate_info_text}.\
+          Gi svaret som en liste med hver kvalifikasjon på en linje."
 
     messages_2 = [
         SystemMessage(content="You are expert in summirizing the key points of CV. "),
-        HumanMessage(content=human_message)
+        SystemMessage(content = f"Provide text in {language} language."),
+        HumanMessage(content=human_message_en if language=="English" else human_message_no)
     ]
 
     qualifications = llm(messages_2).content
     info["qualifications"] = qualifications
 
+    human_message_en = f"Extract the information about the candidate's current position (job) from the provided CV : {candidate_info_text}.\
+                     The response should consist of two to five sentences focusing solely on the candidate's present job role."
+    human_message_no = f"Finn informasjonen om kandidatens nåværende stilling (jobb) fra den gitte CV-en: {candidate_info_text}. \
+        Svaret bør bestå av to til fem setninger som fokuserer utelukkende på kandidatens nåværende jobbrolle."
+
     messages_3 = [
         SystemMessage(content="You are expert in summirizing the key points of CV. "),
-        HumanMessage(content=f"Extract the information about the candidate's current position (job) from the provided CV : {candidate_info_text}.\
-                     The response should consist of two to five sentences focusing solely on the candidate's present job role.\
-                      If using pronouns use he or she basen on the name: {name}. \
-                        Do not use 'they'.")
+        SystemMessage(content = f"Provide text in {language} language."),
+        HumanMessage(content=human_message_en if language=="English" else human_message_no)
     ]
 
     cur_position = llm(messages_3).content
     info["current position"] = cur_position
 
     sleep(60)
+    human_message_en = f"Extract the information about how many years of experience the candidate has from the provided CV : {candidate_info_text}.\
+                     The response should be in the format 'interpreter - 1 month, software developer - 2 years 3 months, data scientist - 1 year.'\
+                     Present time is {__current_time__()}"
+    human_message_no = f"Finn informasjonen om hvor mange års erfaring kandidaten har fra den gitte CV-en: {candidate_info_text}.\
+          Svaret bør være i formatet 'tolk - 1 måned, programvareutvikler - 2 år 3 måneder, data scientist - 1 år.' \
+            Nåværende tidspunkt er {__current_time__()}."
     messages_4 = [
         SystemMessage(content="You are expert in summirizing the key points of CV. "),
-        HumanMessage(content=f"Extract the information about how many years of experience the candidate has from the provided CV : {candidate_info_text}.\
-                     The response should be in the format 'interpreter - 1 month, software developer - 2 years 3 months, data scientist - 1 year.'\
-                     Present time is {__current_time__()}")
+        SystemMessage(content = f"Provide text in {language} language."),
+        HumanMessage(content=human_message_en if language=="English" else human_message_no)
     ]
 
     years = llm(messages_4).content
+    human_message_en = f"Identify which working experience from {years} is relevant for the position with {job_info['position']} with requirements: \
+                         {job_info['key_qualifications']}.\
+                     Summarize the total time of relevant and somehow relevant experience."
+    human_message_no = f"Identifiser hvilken arbeidserfaring fra {years} som er relevant for stillingen med {job_info['position']} \
+        med kravene: {job_info['key_qualifications']}. \
+        Oppsummer den totale tiden med relevant og noenlunde relevant erfaring."
+    system_message_en = "Prioritize depth and breadth of skills and experiences rather than a strict match of job titles. \
+                          Summarize the range of experiences, emphasizing transferable skills and relevant project work."
+    system_message_no = "Prioriter dybden og bredden av ferdigheter og erfaringer heller enn en streng match av stillingstitler. \
+        Oppsummer rekkevidden av erfaringer og legg vekt på overførbare ferdigheter og relevant prosjektarbeid."
     relevant_experience = llm(
         messages=[
             SystemMessage(content="You are a HR that can accurately identify which working experience is relevant for a particular position."),
-            HumanMessage(content=f"Identify which working experience from {years} is relevant for the position with {job_info['position']} with requirements: {job_info['key_qualifications']}.\
-                     Summarize the total time of relevant and somehow relevant experience."),
-            SystemMessage(content="Prioritize depth and breadth of skills and experiences rather than a strict match of job titles. \
-                          Summarize the range of experiences, emphasizing transferable skills and relevant project work.")
+            HumanMessage(content=human_message_en if language=="English" else human_message_no),
+            SystemMessage(content=system_message_en if language=="English" else system_message_no)
         ]
     )
     info["years of experience"] = relevant_experience.content
